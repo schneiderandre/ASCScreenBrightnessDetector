351ea0bf9c8277ffd3d360c10a8db189bb3ddb3b
André Schneider <hello@andreschneider.me>
2014-03-11 14:51:28 +0100

    Add Specta and Expecta to test ASCScreenBrightnessDetector.

---
+++ Example/Pods/Pods-ASCScreenBrightnessDetectorExampleTests-Expecta-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ASCScreenBrightnessDetectorExampleTests_Expecta : NSObject
+@end
+@implementation PodsDummy_Pods_ASCScreenBrightnessDetectorExampleTests_Expecta
+@end
---
+++ Example/Pods/Pods-ASCScreenBrightnessDetectorExampleTests-environment.h
@@ -0,0 +1,26 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// ASCScreenBrightnessDetector
+#define COCOAPODS_POD_AVAILABLE_ASCScreenBrightnessDetector
+#define COCOAPODS_VERSION_MAJOR_ASCScreenBrightnessDetector 0
+#define COCOAPODS_VERSION_MINOR_ASCScreenBrightnessDetector 1
+#define COCOAPODS_VERSION_PATCH_ASCScreenBrightnessDetector 0
+
+// Expecta
+#define COCOAPODS_POD_AVAILABLE_Expecta
+#define COCOAPODS_VERSION_MAJOR_Expecta 0
+#define COCOAPODS_VERSION_MINOR_Expecta 2
+#define COCOAPODS_VERSION_PATCH_Expecta 4
+
+// Specta
+#define COCOAPODS_POD_AVAILABLE_Specta
+#define COCOAPODS_VERSION_MAJOR_Specta 0
+#define COCOAPODS_VERSION_MINOR_Specta 2
+#define COCOAPODS_VERSION_PATCH_Specta 1
+
