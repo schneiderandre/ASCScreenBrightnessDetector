19beff668481decaef506734e71bc19e83060170
André Schneider <hello@andreschneider.me>
2014-03-18 16:39:18 +0100

    Update pods.

--- Example/Pods/Pods-ASCScreenBrightnessDetectorExampleTests-environment.h
+++ Example/Pods/Pods-ASCScreenBrightnessDetectorExampleTests-environment.h
@@ -15,8 +15,8 @@
 // Expecta
 #define COCOAPODS_POD_AVAILABLE_Expecta
 #define COCOAPODS_VERSION_MAJOR_Expecta 0
-#define COCOAPODS_VERSION_MINOR_Expecta 2
-#define COCOAPODS_VERSION_PATCH_Expecta 4
+#define COCOAPODS_VERSION_MINOR_Expecta 3
+#define COCOAPODS_VERSION_PATCH_Expecta 0
 
 // Specta
 #define COCOAPODS_POD_AVAILABLE_Specta
