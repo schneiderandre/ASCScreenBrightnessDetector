d4d3e29a5ed3544aa0e5c2e701aef8a6d3fee216
André Schneider <hello@andreschneider.me>
2014-03-18 21:58:22 +0100

    Test initial state.

--- Example/ASCScreenBrightnessDetectorExampleTests/ASCScreenBrightnessDetectorSpec.m
+++ Example/ASCScreenBrightnessDetectorExampleTests/ASCScreenBrightnessDetectorSpec.m
@@ -20,12 +20,24 @@ describe(@"when newly initialized", ^{
         sut = [ASCScreenBrightnessDetector new];
     });
 
+    it(@"should not be nil", ^{
+        expect(sut).notTo.beNil();
+    });
+
+    it(@"should be the correct Class", ^{
+        expect(sut).to.beKindOf([ASCScreenBrightnessDetector class]);
+    });
+
     it(@"should have a default threshold", ^{
         expect(sut.threshold).to.equal(0.5);
     });
 
     it(@"should have a brightness style", ^{
         expect(sut.screenBrightnessStyle).to.equal(ASCScreenBrightnessStyleDark);
+    });
+
+    it(@"should have a delegate", ^{
+        expect(sut.delegate).to.beNil;
     });
 });
 
